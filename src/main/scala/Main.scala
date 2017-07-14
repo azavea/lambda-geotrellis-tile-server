1d11a53f4e311792de46b1f3179fb16899e5f24f
James <james.santucci@gmail.com>
2017-07-14 14:24:23 -0400

    Support all the ColorRamps

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,7 +3,7 @@ package io.github.jisantuc.gtlambda.lambda
 import io.github.jisantuc.gtlambda.tile._
 
 import com.amazonaws.services.lambda.runtime.{Context, RequestStreamHandler, LambdaLogger}
-import geotrellis.raster.render.{ColorMap, ColorRamps}
+import geotrellis.raster.render.ColorMap
 import io.circe.Json
 import io.circe.optics.JsonPath._
 import io.circe.parser.{parse, decode}
@@ -22,11 +22,7 @@ class TileRequestHandler extends RequestStreamHandler {
     }
     implicit val logger = context.getLogger()
     logger.log(s"$payload")
-    val style = root.s.string.getOption(payloadJson).getOrElse("empty")
-    logger.log(s"Style was: $style")
-    val decoded = style.toLowerCase match {
-      case _ => decode[RgbRequest](payload)
-    }
+    val decoded = decode[DefaultRequest](payload)
     val encodedBytes = decoded match {
       case Right(req) =>
         logger.log(s"Decoded class was ${req.getClass.toString}")
@@ -35,8 +31,13 @@ class TileRequestHandler extends RequestStreamHandler {
             val tile = req.toMultibandTile
             logger.log(s"Class of returned tile was ${tile.getClass.toString}")
             encoder.encode(tile.renderPng.bytes)
-          case _ =>
-            val cm = ColorMap((0 to 3500 by 100).toArray, ColorRamps.Viridis)
+          case "empty" =>
+            val emptyRequest = EmptyRequest(req.x, req.y, req.z)
+            val tile = req.toTile
+            encoder.encode(tile.renderPng.bytes)
+          case s =>
+            val cm = ColorMap((0 to 3500 by 100).toArray,
+                              ColorOptions.fromString(s).getOrElse(ColorOptions.default))
             val tile = req.toTile
             logger.log(s"Class of returned tile was ${tile.getClass.toString}")
             encoder.encode(tile.renderPng(cm).bytes)
